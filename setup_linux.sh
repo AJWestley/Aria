0dc2769777aa1e43fe4a5986075d2b2c39d796c4
AJ <alexanderjwestley@gmail.com>
2023-09-11 01:30:15 +0200

    Removed Setup Files

--- setup_linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python3.9 -m venv aria_env
-source aria_env/bin/activate
-python3 install.py
--- setup_windows.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#python -m spacy download en_core_web_lg
-python3.9 -m venv aria_env
-source aria_env/Scripts/activate
-python3 install.py
