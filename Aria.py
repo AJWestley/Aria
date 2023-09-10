a576b6a9b799c908e1491113d57eb7e5c4cc229d
AJ <alexanderjwestley@gmail.com>
2023-09-10 23:07:38 +0200

    Added main executable

---
+++ Aria.py
@@ -0,0 +1,41 @@
+from Aria.mouth import TTS_Module
+from Aria.brain import Commands
+from Aria.ears import Listener
+from Skills.casual_conversation import introduction
+
+def main():
+    
+    # Init TTS & greet user
+    speaker = TTS_Module()
+    speaker.speak(introduction())
+
+    # Begin listening loop
+    while True:
+        
+        # Wait to be called
+        Listener.listen_for('aria')
+        speaker.speak("I'm listening...")
+
+        # Begin conversation loop
+        while True:
+            
+            # Listen to instruction
+            instruction = Listener.listen()
+
+            # Check for conversation end
+            if instruction in ['nevermind', 'go to sleep']:
+                speaker.speak("Okay. Let me know when you need anything else")
+                break
+            
+            # Understand instruction
+            command, xtra = Commands.parse_command(instruction)
+            
+            # Perform task
+            speaker.speak(Commands.execute_command(command, xtra))
+            
+        # Shut down completely
+        if instruction == 'go to sleep':
+            break
+        
+if __name__ == '__main__':
+    main()
