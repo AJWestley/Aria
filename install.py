26dfc2792674da5248abcdfca36fe7ede3effb9f
AJ <alexanderjwestley@gmail.com>
2023-09-12 14:23:28 +0200

    updated installation file

--- install.py
+++ install.py
@@ -1,7 +1,18 @@
 import os
+import json
+
+def create_key_file():
+    fpath = './data/keys.json'
+    if os.path.exists(fpath):
+        return
+    keys = json.dump({"WEATHER_API_KEY": "key here", "OPEN_AI_KEY": "key here"})
+    with open(fpath, 'w') as keyfile:
+        keyfile.write(keys)
+    
 
 def install():
     os.system("pip install -r requirements.txt")
     
 if __name__ == "__main__":
     install()
+    create_key_file()
